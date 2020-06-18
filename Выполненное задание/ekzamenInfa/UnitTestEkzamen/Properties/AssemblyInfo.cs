fae8d2029f5f283432910c627bdfffd09ffdd33b
TwoTipa <56399523+TwoTipa@users.noreply.github.com>
2020-06-18 11:12:05 +0400

    endProject

---
+++ Выполненное задание/ekzamenInfa/UnitTestEkzamen/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestEkzamen")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestEkzamen")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("594745bf-e0a2-4a4d-8338-14d67cc71d18")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Выполненное задание/ekzamenInfa/ekzamenInfa/xInArray.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ekzamenInfa
+{
+    /// <summary>
+    /// Логика взаимодействия для xInArray.xaml
+    /// </summary>
+    public partial class xInArray : Window
+    {
+        private readonly char[] Separator = { ',', ' ', '.' };
+        private string[] Array;
+        private int Input = 0;
+
+        public xInArray()
+        {
+            InitializeComponent();
+        }
+
+        private void SearchBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if(ArrayInput.Text == null)
+            {
+                MessageBox.Show("Заполните массив");
+            } 
+            else
+            {
+                Array = ArrayInput.Text.Split(Separator);
+                for(var i = 0; i < Array.Length; i++)
+                {
+                    if(Array[i] == "0")
+                    {
+                        Input++;
+                    }
+                }
+                CountZero.Text = Convert.ToString(Input);
+                Input = 0;
+            }
+        }
+
+        private void RandomBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string ret
+            ret = "1,0,1,0,10";
+            ArrayInput.Text = ret;
+        }
+    }
+}
